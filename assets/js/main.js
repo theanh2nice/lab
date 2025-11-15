0289b65f36cf08b01d9d68a796194bcc073f2a86
Theanh <theanh2nice@gmail.com>
2025-11-15 13:17:08 +0000

    Help

--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('#hamburger');
-  btn.addEventListener('click', () => btn.classList.toggle('active'));
-});
